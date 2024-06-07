08f29957309f2f951fb1378896dd57fc8d91a21a
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-07 10:31:47 +0100

    initial html and Javascript sound file pathing

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ console.log("hello there!")
 let hihatSound = new Audio("./sounds/hihat.wav");
 let clapsound = new Audio("./sounds/clap.wav");
 let boomSound = new Audio("./sounds/boom.wav")
-let kickSound = new Audio("")
-let openhatSound = new Audio()
-let rideSound = new Audio()
-let snareSound = new Audio()
-let tinkSound = new Audio()
-let tomSound = new Audio();
+let kickSound = new Audio("./sounds/kick.wav")
+let openhatSound = new Audio("./sounds/openhat.wav");
+let rideSound = new Audio("./sounds/ride.wav");
+let snareSound = new Audio("./sounds/snare.wav");
+let tinkSound = new Audio("./sounds/tink.wav");
+let tomSound = new Audio("./sounds/tom.wav");
 
 
 
